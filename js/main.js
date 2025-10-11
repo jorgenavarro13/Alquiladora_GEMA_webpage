a985c338c100a3d0d61f0dabf418db69f69baa70
JorgePersonal <jorge.85navarro@gmail.com>
2025-10-10 23:07:57 -0600

    Formulario incorporado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+ç
 document.getElementById("contactForm").addEventListener("submit", function(e) {
   e.preventDefault();
-  alert("¡Gracias por contactarnos! Te responderemos pronto.");
+
+  const nombre = document.getElementById("nombre").value;
+  const telefono = document.getElementById("telefono").value;
+  const mensaje = document.getElementById("mensaje").value;
+
+  const numeroDestino = "+527713266491"; // tu número con clave de país (52 = México)
+  const texto = `Hola, soy ${nombre}. Tel: ${telefono}. ${mensaje}`;
+  const url = `https://wa.me/${numeroDestino}?text=${encodeURIComponent(texto)}`;
+
+  window.open(url, "_blank");
   this.reset();
 });
